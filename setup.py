55b1bdb0e7edc892c2ac9bf605b4584e3ce6913f
jdjchelp-jpg <jdjchelp@gmail.com>
2026-03-15 17:02:03 -0500

    ok

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bpm4b",
-    version="9.0.0",
+    version="9.0.2",
     author="Jdjchelp",
     author_email="JDJCHELP@proton.me",
     description="MP3 to M4B Audiobook Converter - Convert MP3 files to M4B format with chapter support",
